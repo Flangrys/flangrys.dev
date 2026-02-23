8e4f28190c7fe8ab6304fcd41f3c3acb1e2efd40
Francisco Matias Prieto Giorgis <prietogiorgisfranciscomatias@gmail.com>
2026-02-23 19:05:44 -0300

    impl: Reimplemented the ExperienceView.tsx using the new card component.

--- src/modules/landing/views/ExperienceView.tsx
+++ src/modules/landing/views/ExperienceView.tsx
@@ -1,30 +1,25 @@
-import Container from "../../ui/components/layout/Container.tsx";
-import {ExperienceCard} from "../components/ExperienceCard.tsx";
+import {CardComponent} from "../components/CardComponent.tsx";
+import {CardsSectionLayout} from "../components/CardsSectionLayout.tsx";
 
 export default function ExperienceView() {
     return (
         <main className="pt-16">
-            <section className="grid grid-cols-4 grid-rows-[auto_1fr] gap-4 py-20">
-                <Container className="col-span-4 header-group border-b border-x-0">
-                    <h3>Experiencia Profesional</h3>
-                    <p className="max-w-2xl">
-                        Roles en los cuales participé activamente en el diseño, desarrollo y evolución
-                        de sistemas backend, arquitectura y prácticas de confiabilidad.
-                    </p>
-                </Container>
-
-                <ExperienceCard
+            <CardsSectionLayout
+                title={"Experiencias Profesionales"}
+                description={"Roles en los cuales participé activamente en el diseño, desarrollo y evolución de sistemas backend, arquitectura y prácticas de confiabilidad."}
+            >
+                <CardComponent
                     heading={"Grupo Penna"}
                     subheading={"FullStack Software Developer"}
                 >
-                    <ExperienceCard.Summary>
-                        Desarrollo de herramientas internas para operaciones logísticas,
+                    <p>
+                        Desarrollé de herramientas internas para operaciones logísticas,
                         enfocadas en la recopilación, validación y procesamiento de datos críticos del negocio.
                         Participé en el diseño de APIs, decisiones de arquitectura y mejoras de observabilidad
                         en sistemas en producción.
-                    </ExperienceCard.Summary>
+                    </p>
 
-                    <ExperienceCard.Experiences items={[
+                    <CardComponent.List items={[
                         "Diseño e implementación de APIs backend con Python y FastAPI.",
                         "Integración de múltiples fuentes de datos (DB, Excel, servicios externos).",
                         "Implementación de logging estructurado, métricas y monitoreo.",
@@ -32,25 +27,25 @@ export default function ExperienceView() {
                         "Colaboración directa con equipos técnicos y áreas del negocio."
                     ]}/>
 
-                    <ExperienceCard.Duration
+                    <CardComponent.Duration
                         from={new Date("2025/09/01")}
                         to={new Date("2026/01/01")}
                     />
-                </ExperienceCard>
+                </CardComponent>
 
 
-                <ExperienceCard
+                <CardComponent
                     heading={"Instituto Nacional de Tecnología Agropecuaria"}
                     subheading={"FullStack Software Engineer"}
                 >
-                    <ExperienceCard.Summary>
+                    <p>
                         Lideré el desarrollo de Agroestim, una aplicación web
                         orientada a la estimación del rendimiento de cultivos de trigo,
                         trabajando end-to-end desde la arquitectura hasta la interfaz de usuario.
-                    </ExperienceCard.Summary>
+                    </p>
 
 
-                    <ExperienceCard.Experiences items={[
+                    <CardComponent.List items={[
                         "Planificación y liderazgo técnico del proyecto.",
                         "Backend con Python, Django y GraphQL.",
                         "Diseño de arquitectura segura y escalable.",
@@ -58,12 +53,12 @@ export default function ExperienceView() {
                         "Frontend en React y Tailwind CSS con componentes reutilizables."
                     ]}/>
 
-                    <ExperienceCard.Duration
+                    <CardComponent.Duration
                         from={new Date("2023/08/01")}
                         to={new Date("2023/12/01")}
                     />
-                </ExperienceCard>
-            </section>
+                </CardComponent>
+            </CardsSectionLayout>
         </main>
     )
 }
