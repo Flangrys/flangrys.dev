2204c300d46cc11b3519ce8cca51e06feb6eaee9
Francisco Matias Prieto Giorgis <prietogiorgisfranciscomatias@gmail.com>
2026-02-24 16:43:08 -0300

    refactor: Rebased project url.

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-    /**
-     * I will disable this `base` configuration in order to use Cloudflare + Github Pages in the future.
-     */
-    base: "/flangrys.dev/",
     plugins: [
         react(),
         tailwindcss(),
