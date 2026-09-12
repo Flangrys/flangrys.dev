50d8c10e8a41d82db132732dc994dc527f5865f6
Francisco Matias Prieto Giorgis <prietogiorgisfranciscomatias@gmail.com>
2026-09-12 12:32:47 -0300

    impl: Added React Query providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,20 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import AppThemeProvider from "./modules/ui/providers/AppThemeProvider.tsx";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import AppRouter from "./router/AppRouter.tsx";
+import AppThemeProvider from "./modules/ui/providers/AppThemeProvider.tsx";
 
 import "./style.css";
 
 const root = document.getElementById('root')!;
 
-
 createRoot(root).render(
     <StrictMode>
-        <AppThemeProvider>
-            <AppRouter/>
-        </AppThemeProvider>
+        <QueryClientProvider client={new QueryClient()}>
+            <AppThemeProvider>
+                <AppRouter/>
+            </AppThemeProvider>
+        </QueryClientProvider>
     </StrictMode>
     ,
 )
